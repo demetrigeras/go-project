47074160a3a47d70a298622cfae4cd56d58d03e2
demetrigeras <demetrijgeras@gmail.com>
2023-11-11 16:13:47 -0500

    backend

--- controllers/tasks.go
+++ controllers/tasks.go
@@ -23,3 +23,37 @@ func CreateTask(c *gin.Context) {
 func GetTasks(c *gin.Context) {
 	c.JSON(http.StatusOK, tasks)
 }
+
+func UpdateTask(c *gin.Context) {
+	id := c.Param("id") // Get the ID from the URL parameter
+	var updatedTask models.Task
+
+	if err := c.BindJSON(&updatedTask); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks[i] = updatedTask // Update the task
+			c.JSON(http.StatusOK, updatedTask)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id") // Get the ID from the URL parameter
+
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...) // Remove the task
+			c.JSON(http.StatusOK, gin.H{"message": "Task deleted"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"go-project/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +16,10 @@ func main() {
 			"message": "Hello World",
 		})
 	})
+	r.POST("/tasks", controllers.CreateTask)       // Create a new task
+	r.GET("/tasks", controllers.GetTasks)          // Retrieve all tasks
+	r.PUT("/tasks/:id", controllers.UpdateTask)    // Update a specific task
+	r.DELETE("/tasks/:id", controllers.DeleteTask) // Delete a specific task
 
 	// Run the server on port 8080
 	r.Run(":8080")
